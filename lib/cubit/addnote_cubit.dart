f15f45a0f6185225b0c9c5f4ac44938980fbf1c7
hagarsalem1 <hagarsalem179@gmail.com>
2023-12-29 15:09:38 +0300

    add note methood to add note cubit

--- lib/cubit/addnote_cubit.dart
+++ lib/cubit/addnote_cubit.dart
@@ -1,11 +1,22 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:meta/meta.dart';
 import 'package:noteapp/Models/Note_model.dart';
+import 'package:noteapp/views/widget/Colors.dart';
 
 part 'addnote_state.dart';
 
 class AddnoteCubit extends Cubit<AddnoteState> {
   AddnoteCubit() : super(AddnoteInitial());
 
-  Addnote(NotesModel note) {}
+  Addnote(NotesModel note) {
+    emit(AddnoteInitialLoading());
+    try {
+      var noteBox = Hive.box<NotesModel>(kNotesBox);
+      emit(AddnoteInitialScussed());
+      noteBox.add(note);
+    } catch (e) {
+      AddnoteInitialFaluire(e.toString());
+    }
+  }
 }
